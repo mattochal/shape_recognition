8dec6196a99602c186653c640d1db308943655b2
Mateusz Ochal <mateuszochal@Mateusz-MacBook-Pro.local>
2017-02-10 16:55:13 +0000

    Pointers

--- crop_images.py
+++ crop_images.py
@@ -5,8 +5,10 @@ import os, glob
 
 #.  from math import abs
 
-path = "/Users/mateuszochal/Documents/University/RoboSoc/images/"
+path = "./PiCam/Images/set1/"
 images_filenames = glob.glob(path + "*.jpg")
+
+print len(images_filenames)
 
 class ExampleApp(tk.Tk):
 	def __init__(self):
@@ -25,6 +27,12 @@ class ExampleApp(tk.Tk):
 		self.ball_count = 0
 		self.cylinder_count = 0
 		self.count = -1
+		self.pressed = False
+		self.cross_hor = self.canvas.create_line(0,0,0,0, width=1, fill=self.outline)
+		self.cross_ver = self.canvas.create_line(0,0,0,0, width=1, fill=self.outline)
+		self.ball_ratio = 1.0/1.0
+		self.cyln_ratio = 63.0/120.0
+		self.crop_points = (0,0,0,0)
 		self.next_image()
 
 	def on_button_press(self, event):
@@ -33,30 +41,50 @@ class ExampleApp(tk.Tk):
 		self.y = event.y
 		
 	def motion(self, event):
+		x0, y0 = self.x, self.y
+		x1, y1 = (event.x, event.y)
+
 		if self.pressed:
-			x0, y0 = self.x, self.y
-			x1,y1 = (event.x, event.y)
 			self.canvas.delete(self.rect)
-			self.rect = self.canvas.create_rectangle(x0,y0,x1,y1, outline=self.outline)
+			if x1 - x0 < 0 and y1 - y0 < 0:
+				dx = max(x1 - x0,y1 - y0)
+				dy = dx
+			elif y1 - y0 < 0:
+				dx = min(x1 - x0, y0 - y1)
+				dy = -dx
+			elif x1 - x0 < 0:
+				dx = -min(x0 - x1, y1 - y0)
+				dy = -dx
+			else:
+				dx = min(x1 - x0,y1 - y0)
+				dy = dx
+			self.crop_points = (x0-dx,y0-dy,x0+dx,y0+dy)
+			self.rect = self.canvas.create_rectangle(x0-dx,y0-dy,x0+dx,y0+dy, outline=self.outline)
+		else:
+			self.canvas.delete(self.cross_hor)
+			self.canvas.delete(self.cross_ver)
+			x0, y0 = self.image.size
+			self.cross_ver = self.canvas.create_line(0,y1,x0,y1, width=1, fill=self.outline)
+			self.cross_hor = self.canvas.create_line(x1,0,x1,y0, width=1, fill=self.outline)
 
 	def on_button_release(self, event):
-		self.pressed = False
-		x0,y0 = (self.x, self.y)
-		x1,y1 = (event.x, event.y)
 		self.canvas.delete(self.rect)
-		# Save image
-		if abs(x0 - x1) > 10 and abs(y1 - y0) > 10 and self.count >= 0:
-			print (x0, y0, x1, y1)
-			crop = self.image.crop((min(x0, x1), min(y0, y1), max(x0, x1), max(y0, y1)))
-			filename = ""
-			if self.cylinder_flag:
-				filename = "cylinders/" + os.path.basename(images_filenames[self.count])[:-4] + "_cyln"+ str(self.cylinder_count) +".jpg"
-				self.cylinder_count += 1
-			else:
-				filename = "balls/" + os.path.basename(images_filenames[self.count])[:-4] + "_ball" + str(self.ball_count)  +".jpg"
-				self.ball_count += 1
-			crop.save(path+filename)
-		self.x = self.y = 0
+		if self.pressed:
+			x0,y0,x1,y1 = self.crop_points
+			# Save image
+			if abs(x0 - x1) > 20 and abs(y1 - y0) > 20 and self.count >= 0:
+				print (x0, y0, x1, y1)
+				crop = self.image.crop((min(x0, x1), min(y0, y1), max(x0, x1), max(y0, y1)))
+				filename = ""
+				if self.cylinder_flag:
+					filename = "cylinders/" + os.path.basename(images_filenames[self.count])[:-4] + "_cyln"+ str(self.cylinder_count) +".jpg"
+					self.cylinder_count += 1
+				else:
+					filename = "balls/" + os.path.basename(images_filenames[self.count])[:-4] + "_ball" + str(self.ball_count)  +".jpg"
+					self.ball_count += 1
+				crop.save(path+filename)
+			self.pressed = False
+		self.crop_points = (0,0,0,0)
 
 	def key_pressed(self, event):
 		if event.char == 'n':
@@ -76,11 +104,15 @@ class ExampleApp(tk.Tk):
 		elif event.char == 'p':
 			if self.count - 2 >= -1:
 				self.count -= 2
-				next_image()
+				self.next_image()
 
+		elif event.char == 'q':
+			if self.pressed:
+				self.pressed = False
+				self.canvas.delete(self.rect)
 
 	def next_image(self):
-		if self.count < len(images_filenames):
+		if self.count+1 < len(images_filenames):
 			self.count += 1
 			image_path = images_filenames[self.count]
 			print (os.path.basename(image_path))
@@ -100,4 +132,30 @@ class ExampleApp(tk.Tk):
 if __name__ == "__main__":
     app = ExampleApp()
     app.mainloop()
+
+
+
+""" Pointer Style 1
+		if self.pressed:
+			self.canvas.delete(self.rect)
+			if x1 - x0 < 0 and y1 - y0 < 0:
+				dx = max(x1 - x0,y1 - y0)
+				dy = dx
+			elif y1 - y0 < 0:
+				dx = min(x1 - x0, y0 - y1)
+				dy = -dx
+			elif x1 - x0 < 0:
+				dx = -min(x0 - x1, y1 - y0)
+				dy = -dx
+			else:
+				dx = min(x1 - x0,y1 - y0)
+				dy = dx
+			self.rect = self.canvas.create_rectangle(x0,y0,x0+dx,y0+dy, outline=self.outline)
+		else:
+			self.canvas.delete(self.cross_hor)
+			self.canvas.delete(self.cross_ver)
+			x0, y0 = self.image.size
+			self.cross_ver = self.canvas.create_line(0,y1,x0,y1, width=1, fill=self.outline)
+			self.cross_hor = self.canvas.create_line(x1,0,x1,y0, width=1, fill=self.outline)
+"""
 
